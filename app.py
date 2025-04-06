da2bd1e407b1d31dd7ee138263c7aee639354a7a
CosmicTurtleUnit <judikael2019@gmail.com>
2025-04-06 12:43:58 -0400

    Integrate MongoDB Atlas for conversion history storage

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 
 from flask import Flask, render_template, request
 from utils import binary_tools
-import sys
 import  os
+from pymongo import MongoClient
 
-#clear the conversion history each each time the app starts
+#clear the conversion history each  time the app starts
 with open("history_file/conversion_history.txt", "w") as file:
     file.write("")
 
+#Flask App Setup
 template_dir = os.path.abspath("templates")
-
+#I
 app = Flask(__name__, template_folder=template_dir)
 
-history = []
+#MongoDB Setup
+MONGO_URI = "mongodb+srv://binaryUser:G@W93@converter.ghsrsp2.mongodb.net/?retryWrites=true&w=majority&appName=Converter"
+client = MongoClient(MONGO_URI)
+db = client['converter']
+history_collection = db['conversion_history']
+
+#history = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -31,29 +38,32 @@ def index():
             elif mode == "bin_to_text":
                 result = binary_tools.binary_to_text(input_data)
 
-        except:
-            result = "Invalid input!"
+            else: result = "Invalid mode"
 
-    return render_template("index.html", result=result, history=history[-5:])
+            #Save MongoDB
+            history_collection.insert_one({
+                "input": input_data,
+                "mode": mode,
+                "result": result
+            })
 
-@app.route("/history")
+        except Exception as e:
+            result = f"Error: {str(e)}"
+
+    return render_template("index.html", result=result) #history=history[-5:])
+
+@app.route("/history", methods=["GET"])
 def view_history():
-    with open("history_file/conversion_history.txt", "r") as f:
-        history = [line.strip() for line in f.readlines()]
+    #with open("history_file/conversion_history.txt", "r") as f:
+    #get the last 10 conversion, newest first.
+    history = list(history_collection.find().sort("_id", -1).limit(10))
     return render_template("history.html", history=history)
-#only used locally - not in production
+
+@app.route("/clear-history", methods=["POST"])
+def clear_history():
+    history_collection.delete_many({})
+    return render_template("history.html", history=[])
+
 if __name__ == "__main__":
-    mode = sys.argv[1] if len(sys.argv) > 1 else "web"
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
-    if mode == "gui":
-        import tkinter as tk
-        from ui.gui import BinaryConverterGUI
-
-        root = tk.Tk()
-        app = BinaryConverterGUI(root)
-        root.mainloop()
-
-    else:
-        from ui.web import run_web_app
-        run_web_app()
-        #app.run()
